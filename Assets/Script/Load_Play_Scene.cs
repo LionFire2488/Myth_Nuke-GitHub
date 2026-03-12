9ec1a0d6d3eee86960df673edaa5026b1535f280
LionFire2488 <waruneeinpheng2517@gmail.com>
2026-03-12 18:17:07 +0700

    G

--- Assets/Script/Load_Play_Scene.cs
+++ Assets/Script/Load_Play_Scene.cs
@@ -3,9 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class Load_Play_Scene : MonoBehaviour
 {
-    public void Load_Scene()
+    public void Load_In_Game_Scene()
     {
         SceneManager.LoadScene("In_Game");
         Score.score = 0;
+    }
+
+    public void Load_Main_Menu_Scene()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+
+    public void Load_Learn_Nuclear_Scene()
+    {
+        SceneManager.LoadScene("LearnNuclear");
+    }
+
+    public void Load_Quiz_Scene()
+    {
+        SceneManager.LoadScene("Quiz");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
