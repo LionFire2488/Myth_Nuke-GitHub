27d6f98518150bdad14ba077e743837434f3fc21
LionFire2488 <waruneeinpheng2517@gmail.com>
2026-03-14 17:16:02 +0700

    add quiz score

--- Assets/Script/Quiz_Score.cs
+++ Assets/Script/Quiz_Score.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class Quiz_Score : MonoBehaviour
 {
-    public static int score = 0;
-    void STM()
+    public static int score;
+
+    void Start()
+    {
+        Debug.Log("score : "+ score);
+    }
+    
+    void Update()
+    {
+        addScore();
+    }
+
+    public void addScore()
     {
         if (Input.GetMouseButtonDown(0))
         {
             score++;
-            Debug.Log("score :" + score);
+            Debug.Log("score : " + score);
         }
     }
 }
